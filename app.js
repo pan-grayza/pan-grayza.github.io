1f102442ffa56f36c0a209686b2a4a1480c6fc9b
Pan-Grayza <zagraystepan@gmail.com>
2022-01-24 23:12:55 +0200

    commit on 'master'

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 */
 
 const script = document.createElement("script")
-script.src = "https://code.jquery.com/jquery-3.4.1.min.js"
+script.src = "https://code.jquery.com/jquery-3.6.0.min.js"
 script.type = "text/javascript"
 document.getElementsByTagName("head")[0].appendChild(script)
 
