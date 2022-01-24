9270ca15bc20e73ff43f487c6168ad564045c594
Pan-Grayza <zagraystepan@gmail.com>
2022-01-24 11:52:02 +0200

    commit on 'master'

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+//
+//
+//
+//
+
+const script = document.createElement("script")
+script.src = "https://code.jquery.com/jquery-3.4.1.min.js"
+script.type = "text/javascript"
+document.getElementsByTagName("head")[0].appendChild(script)
+
+$('input[id="menu-checkbox"]').on("change", function (e) {
+    if ($(this).prop("checked")) {
+        $("body").css("overflow", "hidden")
+    } else {
+        $("body").css("overflow", "auto")
+    }
+})
+
 //If user in dark mode
 
 const isDarkMode = () => {
